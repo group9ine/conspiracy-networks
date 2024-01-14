921b2054522f1c7a65397f8207bd46d513661b07
gulbord <52690645+gulbord@users.noreply.github.com>
2024-01-14 17:04:05 +0100

    Small fix

--- src/utils.R
+++ src/utils.R
@@ -4,9 +4,9 @@ logbins <- function(data, base = 10, dlog = 0.1) {
   counts <- vapply(values, \(x) sum(data == x), integer(1))
 
   # get (log-)midpoints and lower/upper limits (to nearest integer)
-  x <- seq(0, log(max(values), base) + dlog, by = dlog)
-  lower <- ceiling(base^(x - 0.5 * dlog))
-  upper <- floor(base^(x + 0.5 * dlog))
+  log_mid <- seq(0, log(max(values), base) + dlog, by = dlog)
+  lower <- ceiling(base^(log_mid - 0.5 * dlog))
+  upper <- floor(base^(log_mid + 0.5 * dlog))
 
   # select only bins larger than two, e.g. lower = 9, upper = 11
   bin_window <- which(upper > lower + 1)
@@ -16,10 +16,10 @@ logbins <- function(data, base = 10, dlog = 0.1) {
   # take values before bin_window directly from counts
   x <- c(log(seq(1, lower[1] - 1), base), log_mid[bin_window])
   y <- c(
-    counts[vals < lower[1]],  # these are from counts
+    counts[values < lower[1]],  # these are from counts
     # here we average the counts falling inside each remaining bin
     mapply(
-      \(l, u) sum(counts[l <= vals & vals <= u]) / (1 + u - l),
+      \(l, u) sum(counts[l <= values & values <= u]) / (1 + u - l),
       lower, upper
     )
   )
