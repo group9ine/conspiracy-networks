b1cf64c7ee3b86b9575db9a67f748433c1cf9740
gulbord <52690645+gulbord@users.noreply.github.com>
2024-01-14 15:16:24 +0100

    Correct reading example

--- read_fb.R
+++ read_fb.R
@@ -1,11 +1,19 @@
+library(tidyverse)
 library(igraph)
-library(data.table)
 
-fb <- fread(
-  "data/facebook-wall/graph.tsv",
-  sep = " ",
-  col.names = c("from", "to", "count", "time")
-)[rep(seq_len(.N), count), -"count"]  # duplicate rows based on 'count' column
+g <- read_csv("data/facebook-wall/graph.csv", show_col_types = FALSE) |>
+  graph_from_data_frame()
 
-g <- graph_from_data_frame(fb)
-head(E(g)$time)  # time info is there, it seems
+head(E(g)$time)  # time is read correctly
+
+# we can also use actual date + times:
+g <- read_csv("data/facebook-wall/graph.csv", show_col_types = FALSE) |>
+  mutate(time = as_datetime(time, tz = "UTC")) |>
+  graph_from_data_frame()
+head(E(g)$time)
+
+times <- E(g)$time
+
+m <- interval(first(times), last(times)) |>
+  time_length("months")
+sprintf("%d years and %d months", floor(m / 12), floor((m - floor(m)) * 12))
