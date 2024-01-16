f65300bf3cce8b30585423c0a51659d3c7aa40e2
gulbord <52690645+gulbord@users.noreply.github.com>
2024-01-16 14:34:56 +0100

    Edit contagion.R, now it's working

--- contagion.R
+++ contagion.R
@@ -3,37 +3,38 @@ library(ggplot2)
 source("src/utils.R")
 
 dl2 <- read.csv("data/fb-orgs/L2.csv", col.names = c("from", "to"))
-gl2 <- graph_from_data_frame(dl2, directed = FALSE)
+gl2 <- graph_from_data_frame(dl2, directed = FALSE) |> simplify()
+V(gl2)$name <- seq_len(vcount(gl2))  # normalize naming
 
-k <- unname(degree(gl2))
-data <- k
-
-contagion <- function(graph, n_iters, post_prob, dose_window, thresh) {
+contagion <- function(graph, n_iters, c_prob, d_wind, thresh) {
   adj_mat <- igraph::get.adjacency(graph)
   n_nodes <- igraph::vcount(graph)
 
-  doses <- matrix(0, nrow = n_nodes, ncol = dose_window)
+  doses <- matrix(0, nrow = n_nodes, ncol = d_wind)
   inf <- rep(FALSE, n_nodes)
   prev <- rep(0, n_iters)
 
-  for (t in seq_len(dose_window)) {
+  # infect a node at random
+  inf[floor(runif(1, min = 1, max = n_nodes + 1))] <- TRUE
+
+  for (t in seq_len(d_wind)) {
     for (i in seq_len(n_nodes)[inf]) {
-      if (runif(1) < post_prob) {
-        nbs <- neighbors(graph, i, mode = "out")
+      if (runif(1) < c_prob) {
+        nbs <- adj_mat[, i, drop = FALSE]@i + 1
         doses[nbs, t] <- doses[nbs, t] + 1
       }
     }
   }
 
   inf <- rowSums(doses) > thresh
-  prev[dose_window] <- sum(inf)
+  prev[d_wind] <- sum(inf)
 
-  for (t in seq(dose_window, n_iters)) {
+  for (t in seq(d_wind + 1, n_iters)) {
     doses <- cbind(doses[, -1], rep(0, n_nodes))
     for (i in seq_len(n_nodes)[inf]) {
-      if (runif(1) < post_prob) {
-        nbs <- neighbors(graph, i, mode = "out")
-        doses[nbs, dose_window] <- doses[nbs, dose_window] + 1
+      if (runif(1) < c_prob) {
+        nbs <- adj_mat[, i, drop = FALSE]@i + 1
+        doses[nbs, d_wind] <- doses[nbs, d_wind] + 1
       }
     }
 
@@ -41,5 +42,5 @@ contagion <- function(graph, n_iters, post_prob, dose_window, thresh) {
     prev[t] <- sum(inf)
   }
 
-  return(prev)
+  return(prev / n_nodes)
 }
