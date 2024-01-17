9fd759e549c1ab153d60d74f022fc58187c8a41d
gulbord <52690645+gulbord@users.noreply.github.com>
2024-01-17 12:45:59 +0100

    Implement Vulture's edits

--- contagion.R
+++ contagion.R
@@ -10,22 +10,19 @@ dl2 <- read.csv("data/fb-orgs/L2.csv", col.names = c("from", "to"))
 gl2 <- graph_from_data_frame(dl2, directed = FALSE) |> simplify()
 V(gl2)$name <- seq_len(vcount(gl2))  # normalize naming
 
-k <- unname(degree(gl2))
-plot(logbins(k))
-hist(k)
-k
 
-contagion <- function(graph, n_iters, n_inf, c_rate, d_wind, thresh) {
+contagion <- function(
+  graph, n_iters, n_inf, c_rate, d_wind, thresh, display = FALSE
+) {
   degs <- igraph::degree(graph)
   n_nodes <- igraph::vcount(graph)
-  ##[1]
+  rates <- rpois(n_nodes, lambda = c_rate)
   tol <- 5e-3 * n_nodes
 
   doses <- matrix(0, nrow = n_nodes, ncol = d_wind)
   inf <- rep(FALSE, n_nodes)
   prev <- rep(0, n_iters)
 
-  # infect n_inf nodes at random
   pick <- sample.int(n_iters, n_inf, replace = FALSE)
   inf[pick] <- TRUE
   doses[pick, d_wind] <- thresh
@@ -34,24 +31,23 @@ contagion <- function(graph, n_iters, n_inf, c_rate, d_wind, thresh) {
     doses <- cbind(doses[, -1], rep(0, n_nodes))
     for (i in seq_len(n_nodes)[inf]) {
       nbs <- igraph::neighbors(graph, i, mode = "out")
-      nbs <- nbs[degs[i] * runif(length(nbs)) < c_rate] ## replace with [2]
+      nbs <- nbs[degs[i] * runif(length(nbs)) < rates[i]]
       doses[nbs, d_wind] <- doses[nbs, d_wind] + 1
     }
 
     inf <- rowSums(doses) >= thresh
     prev[t] <- sum(inf)
 
-    if (t > 30 && sd(prev[(t - 30):t]) < tol)
-      return(prev[1:t] / n_nodes)
+    if (t > 30 && sd(prev[(t - 30):t]) < tol) {
+      prev <- prev[1:t]
+      break
+    }
   }
 
-  return(prev / n_nodes)
-}
+  prev <- prev / n_nodes
 
-##proposed changes
-## c_rate extracted from certain pdf, capped at c_rate=degs[i]
-##[1] c_rate <- rpois(n=n_nodes,lambda=c_rate_lambda)
-##[2] degs[i] * runif(length(nbs)) < min(degs[i],c_rate[i])
-## optional plot function inside the main function
-## plot = FALSE
-##[3] if(plot == TRUE) {plot(prev / n_nodes)}
+  if (display)
+    plot(prev, type = "l", xlab = "Time step", ylab = "Fractional prevalence")
+
+  return(prev)
+}
