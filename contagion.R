39a2d83063d41b34b488d1df49a0b10a9d8ace74
gulbord <52690645+gulbord@users.noreply.github.com>
2024-01-17 12:02:44 +0100

    Change once again the stopping criterion -- now with sd()

--- contagion.R
+++ contagion.R
@@ -14,7 +14,7 @@ V(gl2)$name <- seq_len(vcount(gl2))  # normalize naming
 contagion <- function(graph, n_iters, n_inf, c_rate, d_wind, thresh) {
   degs <- igraph::degree(graph)
   n_nodes <- igraph::vcount(graph)
-  tol <- 1e-4 * n_nodes
+  tol <- 5e-3 * n_nodes
 
   doses <- matrix(0, nrow = n_nodes, ncol = d_wind)
   inf <- rep(FALSE, n_nodes)
@@ -36,7 +36,7 @@ contagion <- function(graph, n_iters, n_inf, c_rate, d_wind, thresh) {
     inf <- rowSums(doses) >= thresh
     prev[t] <- sum(inf)
 
-    if (t > 30 && dplyr::near(mean(prev[seq(t - 30, t - 1)]), prev[t], tol))
+    if (t > 30 && sd(prev[seq(t - 30, t - 1)]) < tol)
       break
   }
 
