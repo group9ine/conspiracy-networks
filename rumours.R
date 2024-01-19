0457d752df9ea365a7811aff2975b5b730963560
gulbord <52690645+gulbord@users.noreply.github.com>
2024-01-19 19:48:05 +0100

    Experiments with Karate nw + remove Facebook from gcph

--- rumours.R
+++ rumours.R
@@ -30,16 +30,33 @@ ds <- fread(
 df <- fread("data/copenhagen/fb_friends.csv", col.names = c("a", "b"))
 
 # big merge with custom weighting
-dcph <- merge(dc, merge(ds, df, all = TRUE), all = TRUE)[
-  is.na(N.x), N.x := 0][is.na(N.y), N.y := 0][
-      N.x + N.y > 0, weight := 1 + log(N.x + N.y)][
-        N.x + N.y == 0, weight := 1][
-          , c("N.x", "N.y") := NULL]
+# dcph <- merge(dc, merge(ds, df, all = TRUE), all = TRUE)[
+#   is.na(N.x), N.x := 0][is.na(N.y), N.y := 0][
+#       N.x + N.y > 0, weight := 1 + log(N.x + N.y)][
+#         N.x + N.y == 0, weight := 1][
+#           , c("N.x", "N.y") := NULL]
 
-gcph <- graph_from_data_frame(dcph, directed = FALSE) |> simplify()
+# without facebook
+dcph <- merge(dc, ds, all = TRUE)[
+  is.na(N.x), N.x := 0][is.na(N.y), N.y := 0][
+    , .(a, b, weight = 1 + log(N.x + N.y))]
+
+gcph <- graph_from_data_frame(dcph, directed = FALSE) |>
+  simplify() |>
+  get_lcc()
 V(gcph)$name <- seq_len(vcount(gcph))  # normalize naming
 is_simple(gcph)
 clusters(gcph)  # only one cc, good
+
+c <- closeness(gcph)
+c_sc <- (c - min(c)) / (max(c) - min(c))
+n_cols <- 20
+V(gcph)$color <- viridis::plasma(n_cols + 1)[1 + floor(n_cols * c_sc)]
+plot(
+  gcph, layout = layout_with_graphopt(gcph),
+  edge.width = 0.5 * E(gcph)$weight,
+  vertex.size = 2 * k^0.3, vertex.label = NA, vertex.frame.color = NA
+)
 
 # null model
 ger <- sample_gnm(n = vcount(gcph), m = ecount(gcph))
@@ -50,20 +67,21 @@ res_er <- rumour_base(
 )
 res_cph <- rumour_base(
   graph = gcph, n_iters = 100, inf_0 = 1,
-  p_skep = 0.5, spr_rate = 0.5, rec_rate = 0.5,
+  p_skep = 0.2, spr_rate = 0.8, rec_rate = 0.2,
   seed = FALSE, display = TRUE
 )
-res_cph <- rumour_skep(
-  graph = gcph, n_iters = 100, inf_0 = 1,
-  p_talk = 0.6, p_skep = 0.2, p_stop = 0.2,
-  d_wind = 7, thresh = 2,
+gf <- make_full_graph(n = vcount(gcph))
+res_f <- rumour_base(
+  graph = gf, n_iters = 100, inf_0 = 1,
+  p_skep = 0.2, spr_rate = 0.8, rec_rate = 0.1,
   seed = FALSE, display = TRUE
 )
 
 k <- degree(gcph, mode = "out") |> unname()
-k_sr <- rbindlist(
-  lapply(c("sus", "rec"), \(x) data.frame(class = x, k = k[res_cph[[x]]]))
-)[, class := factor(class, c("sus", "rec"))]
+cls <- c("sus", "rec")
+k_sr <- rbindlist(lapply(cls, function(x) {
+    data.frame(class = x, k = k[res_cph[[x]]])
+}))[, class := factor(class, cls)]
 
 ggplot(k_sr, aes(k, fill = class)) +
   geom_histogram(
@@ -71,3 +89,56 @@ ggplot(k_sr, aes(k, fill = class)) +
     binwidth = 1, alpha = 0.5,
     position = "identity", boundary = 0
   )
+
+g <- make_graph("Zachary")
+
+n_sim <- 1000
+nodes <- V(g)
+sims <- matrix(0, ncol = n_sim, nrow = vcount(g))
+for (i in seq_len(n_sim)) {
+  res <- rumour_base(
+    graph = g, n_iters = 100, inf_0 = 1,
+    p_skep = 0.2, spr_rate = 0.8, rec_rate = 0.2,
+    seed = FALSE, display = FALSE
+  )
+
+  sims[nodes[res$sus], i] <- "s"
+  sims[nodes[res$inf], i] <- "i"
+  sims[nodes[res$rec], i] <- "r"
+}
+
+sir_freqs <- rbindlist(
+  apply(
+    sims, 1,
+    \(r) list(sus = sum(r == "s"), inf = sum(r == "i"), rec = sum(r == "r"))
+  )
+)[, lapply(.SD, \(x) x / n_sim)]
+
+# colour by frequency of final state = sus during n_sim runs
+n_cols <- 20
+freqs <- sir_freqs$sus
+hist(freqs)
+cols <- viridis::plasma(n_cols + 1)[
+  1 + floor(n_cols * (freqs - min(freqs)) / (max(freqs) - min(freqs)))]
+plot(
+  g, layout = layout_with_kk(g),
+  vertex.color = cols, vertex.size = 3 * degree(g)^0.4,
+  vertex.label = NA, vertex.frame.color = NA
+)
+
+# colour by final state in sample run
+res <- rumour_base(
+  graph = g, n_iters = 100, inf_0 = 1,
+  p_skep = 0.2, spr_rate = 0.8, rec_rate = 0.5,
+  seed = FALSE, display = TRUE
+)
+V(g)$state <- ifelse(res$sus, "sus", ifelse(res$rec, "rec", "inf"))
+cols <- rep("darkgoldenrod1", vcount(gcph))
+cols[res$inf] <- "firebrick"
+cols[res$rec] <- "dodgerblue4"
+cols[res$start] <- "black"
+plot(
+  g, layout = layout_with_kk(g),
+  vertex.color = cols, vertex.size = 2 * sqrt(degree(g)),
+  vertex.label = NA, vertex.frame.color = NA
+)
