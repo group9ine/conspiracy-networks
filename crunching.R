4e2bf20a781b33c806f051243d0710319c05a90e
gulbord <52690645+gulbord@users.noreply.github.com>
2024-01-25 09:53:32 +0100

    Add Windows version

--- crunching.R
+++ crunching.R
@@ -74,59 +74,40 @@ get_results <- function(spr, rec, func, n_sim = 100) {
 detectCores()
 n_cores <- 4
 
-get_results_par <- function(spr, rec, func, n_sim = 100) {
-  res <- mclapply(
-    seq_len(n_sim),
-    function(i) {
-      if (func == "base") {
-        rumour_base(
-          graph = g, n_iters = 1e4, inf_0 = 1,
-          p_skep = 0, spr_rate = spr, rec_rate = rec,
-          seed = FALSE, display = FALSE
-        )
-      } else {
-        rumour_dose(
-          graph = g, n_iters = 1e4, inf_0 = 1,
-          p_skep = 0, spr_rate = spr, rec_rate = rec, thresh = 2,
-          seed = FALSE, display = FALSE
-        )
-      }
-    },
-    mc.cores = n_cores
-  ) |>
-    Reduce(
-      function(x, y) {
-        list(
-          att_rate = x$att_rate + sum(y$reached) / n_sim,
-          max_inf = x$max_inf + max(y$n_inf) / n_sim,
-          duration = x$duration + y$duration / n_sim,
-          reached = x$reached + 1 * y$reached / n_sim
-        )
-      },
-      x = _,
-      init = list(att_rate = 0, max_inf = 0, duration = 0, reached = 0)
-    )
-
-  res$spr_rate <- spr
-  res$rec_rate <- rec
-
-  return(res)
-}
-
 spr_rates <- c(0.3, 0.7, 0.9)
 rec_rates <- c(0.1, 0.2, 0.3)
 rates <- expand.grid(spr = spr_rates, rec = rec_rates) |>
   transpose()
 setnames(rates, as.character(1:ncol(rates)))
-results <- mclapply(
-  rates, \(x) get_results_par(x[1], x[2], func = "base", n_sim = 500),
-  mc.cores = n_cores
-)
+microbenchmark(
+
+if (Sys.info()["sysname"] %in% c("Linux", "Darwin")) {
+  results <- mclapply(
+    rates, \(x) get_results(x[1], x[2], func = "base", n_sim = 5),
+    mc.cores = n_cores
+  )
+} else {
+  cluster <- makeCluster(n_cores)
+  clusterExport(
+    cluster, c("get_results", "rumour_base", "rumour_dose", "rates", "g")
+  )
+  results <- parLapply(
+    cluster, rates,
+    \(x) get_results(x[1], x[2], func = "dose", n_sim = 5)
+  )
+  stopCluster(cluster)
+}
+
 str(results)
-dput(results, "simulations/base.txt")
+dput(results, "simulations/dose.txt")
 
 
-# previous code
+##################
+# SERIAL VERSION #
+##################
+
+spr_rates <- c(0.3, 0.7, 0.9)
+rec_rates <- c(0.1, 0.2, 0.3)
 results <- outer(
   # spr_rate vector
   spr_rates,
