84a1b118378a4e3fe6c32af9d4d30794cae5c1d5
gulbord <52690645+gulbord@users.noreply.github.com>
2024-01-26 19:27:01 +0100

    Random ramblings

--- analysis.R
+++ analysis.R
@@ -10,6 +10,9 @@ if (Sys.info()["sysname"] == "Darwin") {
   setwd("/Users/lorenzobarbiero/Documents/GitHub/conspiracy-networks/")
 }
 
+g <- read_graph("data/graph_cph.graphml", format = "graphml")
+k <- unname(degree(g))
+
 base1 <- dget("out/base_011.txt") |>
   lapply(t) |>
   do.call(rbind, args = _) |>
@@ -22,6 +25,7 @@ base5 <- dget("out/base_595.txt") |>
 base <- rbind(base1, base5)
 cols <- names(base)[names(base) != "reached"]
 base[, (cols) := lapply(.SD, unlist), .SDcols = cols]
+base$att_rate <- base$att_rate / vcount(g)
 setcolorder(base, c(5, 6, 1:4))
 setorder(base, spr_rate, rec_rate)
 
@@ -39,6 +43,7 @@ dose5 <- dget("out/dose_595.txt") |>
 dose <- rbind(dose1, dose5)
 cols <- names(dose)[names(dose) != "reached"]
 dose[, (cols) := lapply(.SD, unlist), .SDcols = cols]
+dose$att_rate <- dose$att_rate / vcount(g)
 setcolorder(dose, c(5, 6, 1:4))
 setorder(dose, spr_rate, rec_rate)
 
@@ -54,7 +59,8 @@ dose5 <- dose[grep(".[0-9]5$", spr_rate)][grep(".[0-9]5$", rec_rate)]
 base5 |>
   ggplot(aes(spr_rate, rec_rate, fill = att_rate)) +
     scale_fill_viridis_c() +
-    geom_tile()
+    geom_tile() +
+    geom_line(aes(x = spr_rate, y = spr_rate))
 
 dose5 |>
   ggplot(aes(spr_rate, rec_rate, fill = att_rate)) +
@@ -89,8 +95,6 @@ dose5 |>
     scale_fill_viridis_c() +
     geom_tile()
 
-g <- read_graph("data/graph_cph.graphml", format = "graphml")
-k <- unname(degree(g))
 
 plot(k, dose[spr_rate == 0.25 & rec_rate == 0.05, 500 * unlist(reached)])
 
@@ -102,30 +106,39 @@ dose[spr_rate == 0.25 & rec_rate == 0.05,
 
 dose[spr_rate == 0.25 & rec_rate == 0.05]
 
-base5[, .(cor = cor(unlist(reached), k)), keyby = .(spr_rate, rec_rate)] |>
+base5[att_rate > 0.1,
+      .(cor = cor(unlist(reached), k)),
+      keyby = .(spr_rate, rec_rate)] |>
   ggplot(aes(spr_rate, rec_rate, fill = cor)) +
     scale_fill_viridis_c() +
     geom_tile()
 
-dose5[, .(cor = cor(unlist(reached), k)), keyby = .(spr_rate, rec_rate)] |>
+dose5[att_rate > 0.1, .(cor = cor(unlist(reached), k)), keyby = .(spr_rate, rec_rate)] |>
   ggplot(aes(spr_rate, rec_rate, fill = cor)) +
     scale_fill_viridis_c() +
     geom_tile()
 
-base_cor <- base[, .(cor = cor(unlist(reached), k)),
+base_cor <- base[att_rate > 0.1, .(cor = cor(unlist(reached), k), duration),
                  keyby = .(spr_rate, rec_rate)]
-dose_cor <- dose[, .(cor = cor(unlist(reached), k)),
+dose_cor <- dose[att_rate > 0.1, .(cor = cor(unlist(reached), k), duration),
                  keyby = .(spr_rate, rec_rate)]
 
-dose_cor[, .(rec_rate = rec_rate[which.max(cor)], cor = max(cor)),
-         by = spr_rate] |>
-  ggplot(aes(spr_rate, rec_rate)) + geom_point()
+base_bc <- base_cor[cor == max(cor), .(spr = spr_rate, rec = rec_rate)]
+dose_bc <- dose_cor[cor == max(cor), .(spr = spr_rate, rec = rec_rate)]
 
-base_cor[cor == max(cor)]
-dose_cor[cor == max(cor)]
-
-rumour_dose(
+res <- rumour_dose(
   graph = g, n_iters = 1e4, inf_0 = 1,
-  p_skep = 0, spr_rate = 0.25, rec_rate = 0.05, thresh = 5,
+  p_skep = 0, spr_rate = 0.45, rec_rate = 0.35, thresh = 5,
   seed = FALSE, display = TRUE
+)
+V(g)$state <- ifelse(res$sus, "sus", "rec")
+cols <- rep("darkgoldenrod1", vcount(g))
+cols[res$rec] <- "dodgerblue4"
+cols[res$start] <- "olivedrab3"
+size <- 2 * sqrt(k)
+size[res$start] <- 0.5 * max(size)
+plot(
+  g, layout = layout_as_tree(g, root = res$start),
+  vertex.color = cols, vertex.size = 3,
+  vertex.label = NA, vertex.frame.color = NA
 )
